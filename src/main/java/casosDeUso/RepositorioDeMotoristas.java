0582413d3b5c45a7b9fa41c89134fd3ef98f2bcb
gldemichei <gustavodemichei@gmail.com>
2020-05-30 19:50:15 -0300

    repMotoristas aceita multiplos meeeetodos de pagmentos

--- src/main/java/casosDeUso/RepositorioDeMotoristas.java
+++ src/main/java/casosDeUso/RepositorioDeMotoristas.java
@@ -2,8 +2,10 @@ package casosDeUso;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
+import entidades.FormaPgto;
 import entidades.Motorista;
 import entidades.TipoVeiculo;
 import entidades.Veiculo;
@@ -25,14 +27,22 @@ public class RepositorioDeMotoristas {
             String nome = data[1];
             int somatorioAval = Integer.parseInt(data[2]);
             int qtdAval = Integer.parseInt(data[3]);
-            // FOMADEPAGAMENTO = data[4];??
+            String[] aFormaPgto = data[4].split("_");
+            List<FormaPgto> formasPgto = new LinkedList<>();
+            for (String text: aFormaPgto)
+                formasPgto.add(FormaPgto.valueOf(text));
             String veiculoPlaca = data[5];
             String veiculoMarca= data[6];
             String veiculoCor = data[7];
             TipoVeiculo tipo = TipoVeiculo.valueOf(data[8]);
             boolean bagageiroG = Integer.parseInt(data[9]) == 1; // add no construtor
             Veiculo v = new Veiculo(veiculoPlaca, veiculoMarca, veiculoCor, tipo); 
+            v.setBagageiroG(bagageiroG);
             motoristas.add(new Motorista(cpf, nome, v));
         }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        new RepositorioDeMotoristas();
     }
 }
