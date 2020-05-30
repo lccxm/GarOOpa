dacbbad83da47767d2b3794bda4c88615a763f65
lccxm <luccamolon@gmail.com>
2020-05-30 16:03:05 -0300

    padronizacao da area

--- src/main/java/entidades/Roteiro/Cohen.java
+++ src/main/java/entidades/Roteiro/Cohen.java
@@ -1,26 +1,12 @@
 package entidades.Roteiro;
-<<<<<<< HEAD
-
-public class Cohen {
-    public static void main(String[] args) {
-        Ponto p1 = new Ponto(7,9);
-        Ponto p2 = new Ponto(11,4);
-        System.out.println(cohenSutherlandClip(p1, p2));
-=======
-
-import entidades.Roteiro.Area;
-import entidades.Roteiro.Ponto;
-import entidades.Roteiro.Reta;
-import entidades.Roteiro.SituacaoReta;
-
 
 public class Cohen {
     public static void main(String[] args) throws IllegalArgumentException {
-        Ponto p1 = new Ponto(5,5);
-        Ponto p2 = new Ponto(7,7);
+        Ponto p1 = new Ponto(5, 5);
+        Ponto p2 = new Ponto(7, 7);
         Reta rTest = new Reta(p1, p2);
         Ponto p3 = new Ponto(10, 8);
-        Ponto p4 = new Ponto(4,4);
+        Ponto p4 = new Ponto(4, 4);
         Area aTest = new Area(p3, p4);
         Cohen c = new Cohen(aTest, rTest);
         System.out.println(c.cohenSutherlandClip());
@@ -39,19 +25,18 @@ public class Cohen {
 
     private Area a;
     private Reta r;
-    
+
 
     public Cohen(Area a, Reta r) {
         this.a = a;
         this.r = r;
-        xMax = a.getPSupEsq().getX();
-        yMax = a.getPSupEsq().getY();
-        xMin = a.getpInfEsq().getX();
-        yMin = a.getpInfEsq().getY();
+        xMax = a.getPInfEsq().getX();
+        yMax = a.getPInfEsq().getY();
+        xMin = a.getPSupDir().getX();
+        yMin = a.getPSupDir().getY();
     }
 
-
-    public int computeCode(Ponto p){
+     public int computeCode(Ponto p) {
         int code = INSIDE;
         if (p.getX() < xMin)
             code |= LEFT;
@@ -61,23 +46,21 @@ public class Cohen {
             code |= BOTTOM;
         else if (p.getY() > yMax)
             code |= TOP;
-
         return code;
     }
 
-    public SituacaoReta cohenSutherlandClip(){
+    public SituacaoReta cohenSutherlandClip() {
         Ponto p1 = r.getP1();
         Ponto p2 = r.getP2();
         int code1 = computeCode(p1);
         int code2 = computeCode(p2);
         boolean accept = false;
 
-        while (true){
-            if (code1 ==0 && code2 ==0){
+        while (true) {
+            if (code1 == 0 && code2 == 0) {
                 accept = true;
                 break;
-            }
-            else if ((code1 & code2) != 0)
+            } else if ((code1 & code2) != 0)
                 break;
             else {
                 double x = 1.0;
@@ -87,25 +70,24 @@ public class Cohen {
                     codeOut = code1;
                 else
                     codeOut = code2;
-                if ((codeOut & TOP) != 0){
-                    x = p1.getX() + (p2.getX() - p1.getX()) * (yMax - p1.getY()) / (p2.getY() - p1.getY());;
+                if ((codeOut & TOP) != 0) {
+                    x = p1.getX() + (p2.getX() - p1.getX()) * (yMax - p1.getY()) / (p2.getY() - p1.getY());
                     y = yMax;
-                } else if ((codeOut & BOTTOM) != 0){
-                    x = p1.getX() + (p2.getX() - p1.getX()) * (yMin - p1.getY()) / (p2.getY() - p1.getY());;
+                } else if ((codeOut & BOTTOM) != 0) {
+                    x = p1.getX() + (p2.getX() - p1.getX()) * (yMin - p1.getY()) / (p2.getY() - p1.getY());
                     y = yMin;
-                } else if ((codeOut & RIGHT) != 0){
-                    y = p1.getY() + (p2.getY() - p1.getY()) * (xMax - p1.getX()) / (p2.getX() - p1.getX());;
+                } else if ((codeOut & RIGHT) != 0) {
+                    y = p1.getY() + (p2.getY() - p1.getY()) * (xMax - p1.getX()) / (p2.getX() - p1.getX());
                     x = xMax;
-                } else if ((codeOut & LEFT) != 0){
-                    y = p1.getY() + (p2.getY() - p1.getY()) * (xMin - p1.getX()) / (p2.getX() - p1.getX());;
+                } else if ((codeOut & LEFT) != 0) {
+                    y = p1.getY() + (p2.getY() - p1.getY()) * (xMin - p1.getX()) / (p2.getX() - p1.getX());
                     x = xMin;
                 }
-                if (codeOut == code1){
+                if (codeOut == code1) {
                     p1.setX(x);
                     p1.setY(y);
                     code1 = computeCode(p1);
-                }
-                else{
+                } else {
                     p2.setX(x);
                     p2.setY(y);
                     code2 = computeCode(p2);
@@ -118,5 +100,5 @@ public class Cohen {
             return SituacaoReta.TODA_FORA;
     }
 
-        
+
 }
