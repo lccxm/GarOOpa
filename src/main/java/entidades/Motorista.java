f3d34a549eddda80f3e8fae05603eeb938f5ee68
lccxm <luccamolon@gmail.com>
2020-05-30 20:09:37 -0300

    metodo atualiza nota motorista

--- src/main/java/entidades/Motorista.java
+++ src/main/java/entidades/Motorista.java
@@ -4,6 +4,15 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Motorista {
+//    public static void main(String[] args){
+//        Veiculo veiculo = new Veiculo("a","b","azul", TipoVeiculo.LUXO);
+//        Motorista motorista = new Motorista("1","carlos",veiculo);
+//        motorista.atualizaNota(7);
+//        System.out.println(motorista.getNota());
+//        motorista.atualizaNota(7);
+//        System.out.println(motorista.getNota());
+//
+//    }
 
     private String cpf;
     private String nome;
@@ -11,6 +20,7 @@ public class Motorista {
     private int quantAvaliacoes;
     private Veiculo veiculo;
     private List<FormaPgto> formasPgto;
+    private int nota;
 
     public Motorista(String cpf, String nome, Veiculo veiculo){
         this.cpf = cpf;
@@ -40,7 +50,15 @@ public class Motorista {
         this.formasPgto = formasPgto;
     }
 
+    public void atualizaNota(int avaliacao){
+        somaAvaliacoes += avaliacao;
+        quantAvaliacoes += 1;
+        nota = (int)(somaAvaliacoes/quantAvaliacoes);
+    }
 
+    public int getNota(){
+        return nota;
+    }
 
 
 
